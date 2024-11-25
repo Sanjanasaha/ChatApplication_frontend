00074ac199f796cd2064aa0f33a66cad61aa34da
Sanjanasaha <sanjanasaha26@gmail.com>
2024-11-26 02:43:44 +0530

    chatapp-frontend

--- src/hooks/useGetMessages.js
+++ src/hooks/useGetMessages.js
@@ -10,7 +10,7 @@ const useGetMessages = () => {
 		const getMessages = async () => {
 			setLoading(true);
 			try {
-				const res = await fetch(`/api/messages/${selectedConversation._id}`,{
+				const res = await fetch(`https://chatapplication-backend-c1f3.onrender.com/api/messages/${selectedConversation._id}`,{
 					credentials:"include"
 				});
 				const data = await res.json();
--- vite.config.js
+++ vite.config.js
@@ -14,17 +14,25 @@ import react from '@vitejs/plugin-react'
 //   },
 // });
 
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: "https://chatapplication-backend-c1f3.onrender.com",
+//         changeOrigin: true,
+//         secure: false, // Ignore certificate validation
+//       },
+//     },
+//   },
+// });
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://chatapplication-backend-c1f3.onrender.com",
-        changeOrigin: true,
-        secure: false, // Ignore certificate validation
-      },
-    },
-  },
-});
+})
 
 
